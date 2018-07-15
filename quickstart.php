124a7df1cb1baa5093a1d687b5234ae35690cfc9
Efrem Heri Budiarto <ediarto@gmail.com>
2018-07-15 08:24:33 +0700

    Add files via upload

---
+++ quickstart.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * Copyright 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// [START sheets_quickstart]
+require __DIR__ . '/vendor/autoload.php';
+
+/**
+ * Returns an authorized API client.
+ * @return Google_Client the authorized client object
+ */
+function getClient()
+{
+    $client = new Google_Client();
+    $client->setApplicationName('Google Sheets API PHP Quickstart');
+    // $client->setScopes(Google_Service_Sheets::SPREADSHEETS_READONLY);
+    $client->setScopes(Google_Service_Sheets::SPREADSHEETS);
+    $client->setAuthConfig('client_secret.json');
+    $client->setAccessType('offline');
+
+    // Load previously authorized credentials from a file.
+    $credentialsPath = expandHomeDirectory('credentials.json');
+    if (file_exists($credentialsPath)) {
+        $accessToken = json_decode(file_get_contents($credentialsPath), true);
+    } else {
+        // Request authorization from the user.
+        $authUrl = $client->createAuthUrl();
+        printf("Open the following link in your browser:\n%s\n", $authUrl);
+        print 'Enter verification code: ';
+        $authCode = trim(fgets(STDIN));
+
+        // Exchange authorization code for an access token.
+        $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+
+        // Store the credentials to disk.
+        if (!file_exists(dirname($credentialsPath))) {
+            mkdir(dirname($credentialsPath), 0700, true);
+        }
+        file_put_contents($credentialsPath, json_encode($accessToken));
+        printf("Credentials saved to %s\n", $credentialsPath);
+    }
+    $client->setAccessToken($accessToken);
+
+    // Refresh the token if it's expired.
+    if ($client->isAccessTokenExpired()) {
+        $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+        file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
+    }
+    return $client;
+}
+
+/**
+ * Expands the home directory alias '~' to the full path.
+ * @param string $path the path to expand.
+ * @return string the expanded path.
+ */
+function expandHomeDirectory($path)
+{
+    $homeDirectory = getenv('HOME');
+    if (empty($homeDirectory)) {
+        $homeDirectory = getenv('HOMEDRIVE') . getenv('HOMEPATH');
+    }
+    return str_replace('~', realpath($homeDirectory), $path);
+}
+
+// Get the API client and construct the service object.
+$client = getClient();
+$service = new Google_Service_Sheets($client);
+
+// Prints the names and majors of students in a sample spreadsheet:
+// https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+$spreadsheetId = '1sZNray3MJmjicvWmQOy3K_L86W1kQNOaapAMDeg5u34';
+$range = 'Sheet1!A2:E';
+$response = $service->spreadsheets_values->get($spreadsheetId, $range);
+$values = $response->getValues();
+
+if (empty($values)) {
+    print "No data found.\n";
+} else {
+    print "Name, Major:\n";
+    foreach ($values as $row) {
+        // Print columns A and E, which correspond to indices 0 and 4.
+        // echo json_encode($row)."<br/>";
+		if(count($row)>0){
+			echo $row[1]."<br/>";
+		}
+    }
+
+}
+
+
+
+$range = 'Sheet1!A4:E8';
+
+$values = [
+    ["Item", "Cost", "Stocked", "Ship Date"],
+    ["Wheel", "$20.50", "4", "3/1/2016"],
+    ["Door", "$15", "2", "3/15/2016"],
+    ["Engine", "$100", "1", "30/20/2016"],
+];
+
+$updateBody = new Google_Service_Sheets_ValueRange([
+    'range' => $range,
+    'majorDimension' => 'ROWS',
+    'values' => $values,
+]);
+
+$body = new Google_Service_Sheets_ValueRange([
+    'values' => $values
+]);
+
+$valueInputOption = 'USER_ENTERED'; // Or RAW       
+
+$params = [
+  'valueInputOption' => $valueInputOption
+];
+
+$result = $service->spreadsheets_values->update($spreadsheetId, $range,
+$body, $params);
+printf("%d cells updated.", $result->getUpdatedCells());
+
+
+// [END sheets_quickstart]
